5e4ab8a9eeb9a3c16afd90b6705d6038502fa00d
Ajay <ajayds2001@gmail.com>
2023-08-08 00:01:00 +0530

    add: traversal func works as intendend

--- src/paths.rs
+++ src/paths.rs
@@ -1,5 +1,6 @@
-use std::path::{Path, PathBuf};
-
+#![allow(unused)]
+use std::{path::{Path, PathBuf}, str::FromStr};
+use std::convert::AsRef;
 use walkdir::WalkDir;
 
 const STEAM_PATH: &str = "steamapps/common";
@@ -7,31 +8,50 @@ const EPIC_PATH: &str = "Epic Games";
 
 // const ALL_CLIENT_PATHS: Vec<&str> = vec![STEAM_PATH, EPIC_PATH];
 
-enum GameClient {
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum GameClient {
     Steam,
     Epic,
     Other,
 }
 
-struct GameDir {
+#[derive(Debug, PartialEq, Eq)]
+pub struct GameDir  {
     path: PathBuf,
     client: GameClient,
 }
 
 impl GameDir {
-    fn new(path: PathBuf, client: GameClient) -> Self {
+    pub fn new<P: AsRef<Path>>(path: P, client: GameClient) -> Self {
         GameDir {
-            path,
+            path: path.as_ref().to_path_buf(),
             client
         }
     }
+
+    pub fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    pub fn client(&self) -> &GameClient {
+        &self.client
+    }
+
 }
 
-pub fn start_traverse<P>(start_point: P)
+fn get_client_with_loc_str(loc_str: &str) -> GameClient {
+    match loc_str {
+        STEAM_PATH => GameClient::Steam,
+        EPIC_PATH => GameClient::Epic,
+        _ => GameClient::Other
+    }
+}
+
+pub fn start_traverse<P>(start_point: P) -> Vec<GameDir>
 where
     P: AsRef<Path>,
 {
-    let all_client_paths: Vec<&str> = vec![STEAM_PATH, EPIC_PATH];
+    let all_client_paths = vec![STEAM_PATH, EPIC_PATH];
 
     let mut game_paths = Vec::new();
 
@@ -42,9 +62,27 @@ where
     {
         for client in &all_client_paths {
             if entry.path().ends_with(client) {
-                // game_paths.push(GameDir::new(entry.path().to_str(), ))
+                let client = get_client_with_loc_str(client);
+                game_paths.push(GameDir::new(entry.path().to_path_buf(), client));
             }
 
         }
+    }
+
+    game_paths
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::paths::{GameDir, GameClient};
+
+    use super::start_traverse;
+
+    #[test]
+    fn test_paths_exist() {
+        let actual = vec![GameDir::new("/mnt/linux_games/SteamLibrary/steamapps/common", GameClient::Steam)];
+        let res = start_traverse("/");
+        println!("{:?}", res);
+        assert_eq!(res, actual);
     }
 }
